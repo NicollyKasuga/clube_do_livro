11f9c0290f5e753daaada3f5d9a8be429f451e56
Nicky <nicollyporfirio12@gmail.com>
2022-04-26 18:52:02 -0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from app.configs import env_configs, database, migration
+# importar bps...
+
+def create_app():
+
+    app = Flask(__name__)
+
+    env_configs.init_app(app)
+    database.init_app(app)
+    migration.init_app(app)
+
+    # registrar as blueprints...
+
+    return app
