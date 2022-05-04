371518c50a9648532af195f5fa80361a1221920c
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-03 23:08:04 -0300

    feat: added columns first_reader_id and second_reader_id in the table rooms

--- app/models/room_model.py
+++ app/models/room_model.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from uuid import uuid4
-from sqlalchemy import Column
+from sqlalchemy import Column, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from app.configs.database import db
 
@@ -9,7 +9,11 @@ from app.configs.database import db
 class Room(db.Model):
 
     room_id: str
+    first_reader_id: str
+    second_reader_id: str
 
     __tablename__ = "rooms"
 
     room_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    first_reader_id = Column(UUID, ForeignKey("readers.reader_id"), nullable=False)
+    second_reader_id = Column(UUID, ForeignKey("readers.reader_id"), nullable=False)
