4ed18d40c0d506cbf4e2c5452c6f5c86df98e874
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-05-04 16:25:30 -0300

    update: change JWT_SECRET_KEY to SECRET_KEY

--- app/configs/jwt_auth.py
+++ app/configs/jwt_auth.py
@@ -3,5 +3,5 @@ from flask_jwt_extended import JWTManager
 
 
 def init_app(app):
-    app.config["JWT_SECRET_KEY"] = os.getenv("SECRET")
+    app.config["SECRET_KEY"] = os.getenv("SECRET")
     JWTManager(app)
